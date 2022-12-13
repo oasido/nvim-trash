945fef243ca6cb8c1ef1912cac966704f4d5b82e
oasido <asido.ofek@gmail.com>
2022-12-13 23:40:52 +0200

    edit better-escape setup function

--- lua/config/better-escape.lua
+++ lua/config/better-escape.lua
@@ -1,5 +1,8 @@
 import("better_escape", function(betterEscape)
 	betterEscape.setup({
 		mapping = { "jk", "jj" },
+		timeout = vim.o.timeoutlen,
+		clear_empty_lines = false,
+		keys = "<Esc>",
 	})
 end)
